a002ca51e4e61e1d6f5ddc074551980577c78c2a
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2020-06-06 22:37:08 +0530

    Make "algor_config" as external.
    "struct algor_conf algor_config" will be used by every source file. Hence
    make it extern.
    
    Signed-off-by: Vaibhav Gupta <vaibhavgupta40@gmail.com>

--- algor.h
+++ algor.h
@@ -1,4 +1,5 @@
 #include <limits.h>
+#include <stdint.h>
 
 /* ========================================================================= */
 
@@ -12,12 +13,14 @@
      * fgets(char*, size_t, stream), reads size_t-1 no of characters from
      * stream or until newline character is found.
      */
-    struct {
+    struct algor_config_t {
         char cwd[PATH_MAX];      /* Current Working Directory */
         char questwd[PATH_MAX];  /* Question Working Directory */
         char testwd[PATH_MAX];   /* Test Working Directory */
         char compwd[PATH_MAX];   /* Compiled Working Directory */
-    } extern algor_config;
+        uint8_t flags:6;
+    };
+    extern struct algor_config_t algor_config;
 
 /* ===================== No Declarations Beyond This ======================= */
 
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* Standard Headers */
-#include <stdint.h>
+#include <argp.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -9,9 +9,24 @@
 #include "dformat.h"
 #include "functions.h"
 
-int main (int argc, char *argv[]){
-    char a[100];
+/* ========================================================================= */
+struct algor_config_t algor_config = {
+    .cwd = "-",
+    .questwd = "-",
+    .testwd = "-",
+    .compwd = "-",
+    .flags = 0x0
+};
+/* ========================================================================= */
 
+
+int main (int argc, char *argv[]){
+    extern struct algor_config_t algor_config;
+    char a[100];
+    printB("%s\n%s\n%s\n%s\n", algor_config.cwd,
+                               algor_config.questwd,
+                               algor_config.testwd,
+                               algor_config.compwd);
     printf("Enter a string: ");
     getsB(a, 100);
     printBH("%s",a);
