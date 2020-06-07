27d86e2da178d99b68b6b78b01fa9252d56e358d
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2020-06-07 19:19:26 +0530

    Add Parser
    Algor can now parse commandline arguments.
    
    Signed-off-by: Vaibhav Gupta <vaibhavgupta40@gmail.com>

--- algor.h
+++ algor.h
@@ -8,19 +8,46 @@
 
 /* ====================== All Declarations Below This ====================== */
 
-    /* ---- Configuration --------------------------------------------------
+/* ============================ CONFIGURATION ============================== */
+    /*
+     * Structure to contain user configuration.
      *
-     * fgets(char*, size_t, stream), reads size_t-1 no of characters from
-     * stream or until newline character is found.
+     * We use "opt" as a set of bit flags to know which all options were
+     * passed in terminal with algor. The values of those options get stored
+     * in the object "algor_config" of structure "struct algor_config_t".
+     * 
+     * Value of opt when bitwised &ed with 0x1, 0x2, 0x4, 0x8, 0x10, 0x20:
+     * - 1  = (0x1)  = show
+     * - 2  = (0x2)  = test
+     * - 4  = (0x4)  = lang
+     * - 8  = (0x8)  = quest
+     * - 16 = (0x10) = flag
+     * - 32 = (0x20) = write
      */
     struct algor_config_t {
         char cwd[PATH_MAX];      /* Current Working Directory */
         char questwd[PATH_MAX];  /* Question Working Directory */
         char testwd[PATH_MAX];   /* Test Working Directory */
         char compwd[PATH_MAX];   /* Compiled Working Directory */
-        uint8_t flags:6;
+        uint8_t opt:6;
     };
     extern struct algor_config_t algor_config;
+
+/* ============================== Arguments =================================*/
+
+    /*
+     * Used by main to communicate with parse_opt.
+     * The values passed with options, on terminal, are stored in these. 
+     */
+    struct algor_arguments_t {
+        char *quest_file; /* Pointer to value passed with '-q' or '-s' or
+                             '--quest' -r '--show'*/
+        char *test_file;  /* Pointer to value passed with '-t' or '--test' */
+        char *lang;       /* Pointer to value passed with '-l' or '--lang' */
+        char *flag;       /* Pointer to value passed with '-f' or '--flag' */
+    };
+    extern struct algor_arguments_t algor_arguments;
+
 
 /* ===================== No Declarations Beyond This ======================= */
 
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 /* Standard Headers */
 #include <argp.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -9,32 +11,164 @@
 #include "dformat.h"
 #include "functions.h"
 
-/* ========================================================================= */
+/* ============================= ALGOR CONFIG ============================== */
+
 struct algor_config_t algor_config = {
     .cwd = "-",
     .questwd = "-",
     .testwd = "-",
     .compwd = "-",
-    .flags = 0x0
+    .opt = 0x0
 };
-/* ========================================================================= */
 
+/* =========================== ALGOR ARGUMENTS ============================= */
 
-int main (int argc, char *argv[]){
+struct algor_arguments_t algor_arguments = {
+    .quest_file = "-",
+    .test_file  = "-",
+    .lang = "-",
+    .flag = "-"
+};
+
+/* ============================= ALGOR OPTIONS ============================= */
+
+#define OPT_DOC "Documentation.\n"
+#define OPT_FLAG "Programs sometimes requires additional flags, either during"\
+                 "run-time or compile-time. For example, C/C++ programs may"\
+                 "require flags like \"-lm\", \"-lpthread\", \"-lncurses\", "\
+                 "etc. during compile-time. Python may require \"-c\", \"-q\""\
+                 ", etc. during runtime. They should be written inside double"\
+                 " quotes.\n"
+
+#define OPT_LANG "Specify the language of your program. LANG=\n"\
+                 "[C, C++, C++11, C++17, JAVA, Python2, Python3,\n"\
+                 "Rust, Javascript, etc].\n"
+
+#define OPT_QUEST "Used with \"--test\" for evaluation. Specify file-name or "\
+                  "full-path to the question file.\n"
+
+#define OPT_SHOW "Prints question. Specify file-name or full-path to the "\
+                 "question file.\n"
+
+#define OPT_TEST "Program/Solution File. Specify file-name or full-path to the"\
+                 "test/solution file.\n"
+
+#define OPT_WRITE "Write a new question. Specify file-name or full-path of the"\
+                  "Question-file, to be made.\n"
+
+static const struct argp_option algor_options[] = {
+    {"doc",   'd',           0,                   0, OPT_DOC},
+    {"flag",  'f',      "FLAG",                   0, OPT_FLAG},
+    {"lang",  'l',      "LANG",                   0, OPT_LANG},
+    {"quest", 'q', "FILE/PATH", OPTION_ARG_OPTIONAL, OPT_QUEST},
+    {"show",  's', "FILE/PATH", OPTION_ARG_OPTIONAL, OPT_SHOW},
+    {"test",  't', "FILE/PATH",                   0, OPT_TEST},
+    {"write", 'w', "FILE/PATH", OPTION_ARG_OPTIONAL, OPT_WRITE},
+    { 0 }
+};
+
+#undef OPT_DOC
+#undef OPT_FLAG
+#undef OPT_LANG
+#undef OPT_QUEST
+#undef OPT_SHOW
+#undef OPT_TEST
+#undef OPT_WRITE
+
+/* =========================== ARGP DEFINITIONS ============================ */
+
+const char *argp_program_version = "AlgoR - Offline Judge : 0.0.1";
+static const char usage_doc[] = "\nAlgoR - Offline Judge";
+
+/* ============================== ARGP PARSER ============================== */
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state){
+    /* Get the input argument from argp_parse, which we
+       know is a pointer to our arguments structure.*/
+
     extern struct algor_config_t algor_config;
-    char a[100];
-    printB("%s\n%s\n%s\n%s\n", algor_config.cwd,
-                               algor_config.questwd,
-                               algor_config.testwd,
-                               algor_config.compwd);
-    printf("Enter a string: ");
-    getsB(a, 100);
-    printBH("%s",a);
+    struct algor_arguments_t *arguments = state->input;
+    uint32_t i;
 
-    printH("%s", "Hello");
-    printB("%s", "Hello");
-    printBH("%s", "Hello");
-    print_error("%s", "testing");
+    switch(key){
+        case 'd':
+            system("less documentation");
+            break;
+
+        case 'f':
+            arguments->flag = arg;
+            algor_config.opt |= 0x10;
+            break;
+
+        case 'l':
+            for(i=0; arg[i] != '\0'; i++)
+                arg[i] = tolower(arg[i]);
+            arguments->lang = arg;
+            algor_config.opt |= 0x4;
+            break;
+
+        case 'q':
+            if(arg && arguments->quest_file[0] == '-')
+                arguments->quest_file = arg;
+            algor_config.opt |= 0x8;
+            break;
+
+        case 's':
+            if(arg && arguments->quest_file[0] == '-')
+                arguments->quest_file = arg;
+            algor_config.opt |= 0x1;
+            break;
+
+        case 't':
+            arguments->test_file = arg;
+            algor_config.opt |= 0x2;
+            break;
+
+        case 'w':
+            if(arg)
+                arguments->quest_file = arg;
+            algor_config.opt |= 0x20;
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
 
     return 0;
 }
+
+/* ========================================================================= */
+
+static struct argp algor_argp = { algor_options, parse_opt, 0, usage_doc };
+
+/* ========================================================================= */
+
+int main (int argc, char *argv[]){
+    extern struct algor_arguments_t algor_arguments;
+
+    if(argc == 1){
+        fprintf(stderr, "algor: Too few arguments"
+                        "\n%s\n"
+                        "\nTry `algor --help` or `algor --usage` for more "
+                        "information.\n", usage_doc);
+        return 1;
+    }
+
+    /* Parse the commandline arguments. */
+    argp_parse(&algor_argp, argc, argv, ARGP_NO_ARGS, 0, &algor_arguments);
+
+    print("%s", "");
+
+    print("%s\n%s\n%s\n%s\n%u", algor_arguments.quest_file,
+                              algor_arguments.test_file,
+                              algor_arguments.lang,
+                              algor_arguments.flag,
+                              algor_config.opt);
+
+
+
+    return 0;
+}
+
+/* ========================================================================= */
